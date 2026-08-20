e3f8a71309ebcc3bec8d649a97201f9b722a83c5
tahirazeem145 <tahirazeems145s@gmail.com>
2026-08-20 20:54:30 +0530

    test: add integration tests for deployment sequence handling and build concurrency limits

--- backend/src/test/java/com/azhost/service/ProductionHardeningConcurrencyIntegrationTest.java
+++ backend/src/test/java/com/azhost/service/ProductionHardeningConcurrencyIntegrationTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.Path;
 import java.time.ZonedDateTime;
